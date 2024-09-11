4d6466b07cc8eb46f9af2547d72ceb2b61697626
Kevin SM <coolkevino@gmail.com>
2024-09-11 18:44:31 -0400

    updating multiple things

--- csce242/assignments/assignment-5/script.js
+++ csce242/assignments/assignment-5/script.js
@@ -1,6 +1,6 @@
 //author: Kevin Snyder-Mounts
 
-let val = '${slider.value}px';
+let val = slider.value+"px";
 document.getElementById("main-slider").oninput = ()=>{
     document.getElementById("slider-image").style.left = val;
 }
