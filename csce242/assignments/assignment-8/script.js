2d005be5aad3cd4c26277768b748563daf9db27d
Kevin SM <coolkevino@gmail.com>
2024-09-26 20:41:00 -0400

    updating assignment-8

--- csce242/assignments/assignment-8/script.js
+++ csce242/assignments/assignment-8/script.js
@@ -4,19 +4,26 @@ const createTd = (data)=>{
     return td;
 }
 
-const birthday = document.createElement("img").src = "images/birthday.jpg";
-const clown = document.createElement("img").src = "images/clown.jpg";
-const rain = document.createElement("img").src = "images/rain.jpg";
-const read = document.createElement("img").src = "images/read.jpg";
-const shovel = document.createElement("img").src = "images/shovel.jpg";
-const work = document.createElement("img").src = "images/work.jpg";
+const birthday = document.createElement('img');
+const clown = document.createElement('img');
+const rain = document.createElement('img');
+const read = document.createElement('img');
+const shovel = document.createElement('img');
+const work = document.createElement('img');
+
+birthday.src = "images/birthday.jpg";
+clown.src = "images/clown.jpg";
+rain.src = "images/rain.jpg";
+read.src = "images/read.jpg";
+shovel.src = "images/shovel.jpg";
+work.src = "images/work.jpg";
 
 const birthdayInfo = []
 birthdayInfo[0]= "birthday";
 birthdayInfo[1]= "Happy Birthday! It's the best/worst day of the year";
 const clownInfo = []
 clownInfo[0]="clown";
-clownInfo[1]= "Clowns are supposed to be funny and entertaining. i find them either really creepy or just flat out stupid.";
+clownInfo[1]= "Clowns are supposed to be funny and entertaining. i find them really creepy.";
 const rainInfo = []
 rainInfo[0]= "rain";
 rainInfo[1]= "Rain rain go away come again another day. Idk I like the rain though.";
@@ -42,10 +49,11 @@ const table = document.getElementById("img-table")
 
 console.log(imgs);
 
-for (let img in imgs){
+for(let img in imgs){
     let tr = document.createElement("tr");
+    table.append(tr);
     const td = createTd(img);
-    tr.append(td);
+    tr.appendChild(td);
     td.onclick = ()=>{
         document.getElementById("title").innerHTML = imgs[img][0];
         document.getElementById("desc-txt").innerHTML = imgs[img][1];
