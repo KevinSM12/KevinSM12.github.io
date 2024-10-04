ff2ebd780ee67f275692bb2f1b3575331da8f8d3
Kevin SM <coolkevino@gmail.com>
2024-10-04 14:11:59 -0400

    updating assignment-9

--- csce242/assignments/assignment-9/script.js
+++ csce242/assignments/assignment-9/script.js
@@ -19,21 +19,20 @@ class Bird {
         section.append(columnsDiv);
         const firstColumn = document.createElement("div");
         columnsDiv.append(firstColumn);
-        const secondColumn = document.createElement("div");
-        columnsDiv.append(secondColumn);
+        const popOut = document.createElement("div");
 
         firstColumn.append(this.picture(this.pic));
 
-        secondColumn.classList.add("hidden");
-        secondColumn.append(this.paragraph("Habitat", this.habitat));
-        secondColumn.append(this.paragraph("Life span", this.lifeSpan));
-        secondColumn.append(this.paragraph("Diet", this.food));
-        secondColumn.append(this.paragraph("Size", this.size));
+        popOut.classList.add("hidden");
+        popOut.append(this.paragraph("Habitat", this.habitat));
+        popOut.append(this.paragraph("Life span", this.lifeSpan));
+        popOut.append(this.paragraph("Diet", this.food));
+        popOut.append(this.paragraph("Size", this.size));
 
         section.onclick = () => {
             secondColumn.classList.toggle("hidden");
         };
-        
+
         return section;
     }
 
@@ -51,10 +50,10 @@ class Bird {
 }
 
 const birds = [];
-birds.push(new Bird("Parrot","Tropical","40-60 years","seeds, fruit, and anthropods","40 inches","csce242/assignments/assignment-9/images/parrot.jpg"));
-birds.push(new Bird("Red Junglefowl","Tropical","12-15 years","fruits and seeds","28 inches","csce242/assignments/assignment-9/images/red-junglefowl.jpg"));
-birds.push(new Bird("Burrowing Owl","Western US","6-8 years","large insects and small rodents","7-11 inches","csce242/assignments/assignment-9/images/Burrowing-owl.jpg"));
-birds.push(new Bird("Blue Jay","Eastern and Central US","7 years","nuts, corn, and grain","9-12 inches","csce242/assignments/assignment-9/images/blue-jay.jpg"));
+birds.push(new Bird("Parrot","Tropical","40-60 years","seeds, fruit, and anthropods","40 inches","parrot.jpg"));
+birds.push(new Bird("Red Junglefowl","Tropical","12-15 years","fruits and seeds","28 inches","red-junglefowl.jpg"));
+birds.push(new Bird("Burrowing Owl","Western US","6-8 years","large insects and small rodents","7-11 inches","Burrowing-owl.jpg"));
+birds.push(new Bird("Blue Jay","Eastern and Central US","7 years","nuts, corn, and grain","9-12 inches","blue-jay.jpg"));
 
 birds.forEach((bird)=>{
     document.getElementById("bird-list").append(bird.item);
