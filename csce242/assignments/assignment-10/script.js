f9f691f24285f7b9b696981c48af3ea127a3bcc2
Kevin SM <coolkevino@gmail.com>
2024-10-09 20:44:42 -0400

    updating assignment -10

--- csce242/assignments/assignment-10/script.js
+++ csce242/assignments/assignment-10/script.js
@@ -10,10 +10,19 @@ const getFlavors = async() => {
 
 const showFlavors = async() => {
     const flavors = await getFlavors();
-    const flavorsSection = document.getElementById("flavors");
+    const flavorsSection1 = document.getElementById("flavors1");
+    const flavorsSection2 = document.getElementById("flavors2");
+    let count=0;
 
     flavors.forEach((flavor)=>{
-        flavorsSection.append(getFlavor(flavor));
+        if(count%2 === 0){
+            console.log(count);
+            flavorsSection1.append(getFlavor(flavor));
+        }else{
+            console.log(count);
+            flavorsSection2.append(getFlavor(flavor));
+        }
+        count++;
     });
 };
 
@@ -21,15 +30,18 @@ const showFlavors = async() => {
 const getFlavor = (flavor)=>{
     const section = document.createElement("section");
     section.classList.add("container","one");
-    //section.style.backgroundImage = "https://portiaportia.github.io/json/images/ice-creams/"+flavor.image;
+    const img = document.createElement("img");
+    img.src = `https://portiaportia.github.io/json/images/ice-creams/${flavor.image}`;
+section.append(img);
 
     const title = document.createElement("section");
-    title.classList.add("overlay","hidden");
+    title.classList.add("overlay");
     section.append(title);
     const p = document.createElement("p");
     p.innerHTML=flavor.name;
     title.append(p);
 
+    return section;
 }
 
 showFlavors();
