767984f79f2786d886676e1754d3ed021bd8c51d
Kevin SM <coolkevino@gmail.com>
2024-10-10 17:43:21 -0400

    updating part 6

--- csce242/projects/part6/script.js
+++ csce242/projects/part6/script.js
@@ -1,3 +1,32 @@
 document.getElementById("toggle-nav").onclick = ()=>{
     document.getElementById("nav-items").classList.toggle("hide");
 }
+
+const getRecords = async() => {
+    const url = "csce242/json/record.json";
+
+    try {
+        const response = await fetch(url);
+        return response.json();
+    } catch(error){
+        console.log(error);
+    }
+}
+
+//shows the list of shoes
+const showRecords = async() => {
+    const records = await getRecords();
+    const recordsSection = document.getElementById("records-section");
+
+    records.forEach((record)=>{
+        recordsSection.append(getRecord(record));
+    });
+};
+
+const getRecord = (record) => {
+    const section = document.createElement("section");
+    
+    return section;
+}
+
+showShoes();
