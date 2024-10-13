5c1673b8d6780c76e9efca6244ef73ceb77cc2fc
Kevin SM <coolkevino@gmail.com>
2024-10-13 15:53:22 -0400

    updating record.json

--- csce242/projects/part6/records.js
+++ csce242/projects/part6/records.js
@@ -48,13 +48,39 @@ const getRecord = (record) => {
         h2.innerHTML = record.record_title;
         content.append(h2);
         const recordHolders = document.createElement("div");
-        recordHolders.classList.add("columns");
+        recordHolders.classList.add("columns","record-holders");
         content.append(recordHolders);
         const holderSection = document.createElement("section");
-        holderSection.classList.add("one");
+        holderSection.classList.add("one","holder-section");
         recordHolders.append(holderSection);
         const holderImg = document.createElement("img");
-        holderImg.src = record.record.record-holder-image;
+        holderImg.src = record.record.record_holder_image;
+        const h4 = document.createElement("h4");
+        h4.innerHTML = "Current Record Holder:"
+        holderSection.append(h4);
+        const h3 = document.createElement("h3")
+        h3.innerHTML = record.record.record_holder;
+        holderSection.append(h3);
+        const p = document.createElement("p");
+        p.innerHTML = record.record.record_holder_description;
+        holderSection.append(p);
+        const prevHolderSection = document.createElement("section");
+        prevHolderSection.classList.add("one","holder-section");
+        recordHolders.append(prevHolderSection);
+        const prevHolderImg = document.createElement("img");
+        prevHolderImg.src = record.record.prev_record_holder_image;
+        prevHolderSection.append(prevHolderImg);
+        const prevh4 = document.createElement("h4");
+        prevh4.innerHTML = "Previous Record Holder:"
+        prevHolderSection.append(prevh4);
+        const prevh3 = document.createElement("h3")
+        prevh3.innerHTML = record.record.prev_record_holder;
+        prevHolderSection.append(prevh3);
+        const prevp = document.createElement("p");
+        prevp.innerHTML = record.record.prev_record_holder_description;
+        prevHolderSection.append(prevp);
+
+        return modal;
     }
 
     return section;
