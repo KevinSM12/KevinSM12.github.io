20c507c7ceaaa1b426f9e240852e44c146cf112d
Kevin SM <coolkevino@gmail.com>
2024-10-13 18:13:43 -0400

    updating records.js

--- csce242/projects/part6/records.js
+++ csce242/projects/part6/records.js
@@ -25,18 +25,17 @@ const showRecords = async() => {
 
 const getRecord = (record) => {
     const section = document.createElement("section");
-    document.getElementById("records").append(section);
     section.classList.add("one");
     const recordDiv = document.createElement("div");
     recordDiv.classList.add("record-section");
     section.append(recordDiv);
     const img = document.createElement("img");
-    img.src = this.record_image;
+    img.src = record.record_image;
     recordDiv.append(img);
     const title = document.createElement("h4");
-    title.innerHTML = this.record_title;
+    title.innerHTML = record.record_title;
     const desc = document.createElement("p");
-    desc.innerHTML = this.record_description;
+    desc.innerHTML = record.record_description;
 
     section.onclick = ()=>{
         const modal = document.createElement("div");
@@ -45,7 +44,7 @@ const getRecord = (record) => {
         content.classList.add("modal-content");
         modal.append(content);
         const h2 = document.createElement("h2");
-        h2.innerHTML = this.record_title;
+        h2.innerHTML = record.record_title;
         content.append(h2);
         const recordHolders = document.createElement("div");
         recordHolders.classList.add("columns","record-holders");
@@ -54,30 +53,30 @@ const getRecord = (record) => {
         holderSection.classList.add("one","holder-section");
         recordHolders.append(holderSection);
         const holderImg = document.createElement("img");
-        holderImg.src = this.record_holder_image;
+        holderImg.src = record.record_holder_image;
         const h4 = document.createElement("h4");
         h4.innerHTML = "Current Record Holder:"
         holderSection.append(h4);
         const h3 = document.createElement("h3")
-        h3.innerHTML = this.record_holder;
+        h3.innerHTML = record.record_holder;
         holderSection.append(h3);
         const p = document.createElement("p");
-        p.innerHTML = this.record_holder_description;
+        p.innerHTML = record.record_holder_description;
         holderSection.append(p);
         const prevHolderSection = document.createElement("section");
         prevHolderSection.classList.add("one","holder-section");
         recordHolders.append(prevHolderSection);
         const prevHolderImg = document.createElement("img");
-        prevHolderImg.src = this.prev_record_holder_image;
+        prevHolderImg.src = record.prev_record_holder_image;
         prevHolderSection.append(prevHolderImg);
         const prevh4 = document.createElement("h4");
         prevh4.innerHTML = "Previous Record Holder:"
         prevHolderSection.append(prevh4);
         const prevh3 = document.createElement("h3")
-        prevh3.innerHTML = this.prev_record_holder;
+        prevh3.innerHTML = record.prev_record_holder;
         prevHolderSection.append(prevh3);
         const prevp = document.createElement("p");
-        prevp.innerHTML = this.prev_record_holder_description;
+        prevp.innerHTML = record.prev_record_holder_description;
         prevHolderSection.append(prevp);
 
         return modal;
